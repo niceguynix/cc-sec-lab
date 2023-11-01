8f575af847c9075d797dadd81094b7ae4b4c2dd3
niceguynix <its200manoj@gmail.com>
2023-11-01 21:42:36 +0530

    rsa

--- Rsa.java
+++ Rsa.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.math.*;
+import java.math.BigInteger;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,19 +6,23 @@ public class Rsa {
   static Scanner sc = new Scanner(System.in);
   public static void main(String[] args) {
     System.out.print("Enter a Prime number: ");
-    BigInteger p = sc.nextBigInteger(); // one primenumber 
+    BigInteger p = sc.nextBigInteger(); // one primenumber
     System.out.print("Enter another prime number: ");
-    BigInteger q = sc.nextBigInteger(); // another primenumber 
+    BigInteger q = sc.nextBigInteger(); // another primenumber
+    
     BigInteger n = p.multiply(q);
     BigInteger n2 =
-    p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
     BigInteger e = generateE(n2);
-    BigInteger d = e.modInverse(n2); // Here's themultiplicative inverse 
+    BigInteger d = e.modInverse(n2); // Here's themultiplicative inverse
+    
     System.out.println("Encryption keys are: " + e + ", " + n);
     System.out.println("Decryption keys are: " + d + ", " + n);
     System.out.print("Enter message: ");
+    
     BigInteger m = sc.nextBigInteger();
     BigInteger c = encrypt(m, e, n);
+
     System.out.println("Ciphertext: " + c);
     System.out.println("Plaintext: " + decrypt(c, d, n));
   }
@@ -30,22 +32,18 @@ public class Rsa {
     BigInteger gcd;
     Random x = new Random();
     do {
-      y = x.nextInt(fiofn.intValue() - 1);
+      y = x.nextInt(fiofn.intValue() + 1);
       String z = Integer.toString(y);
       e = new BigInteger(z);
       gcd = fiofn.gcd(e);
       intGCD = gcd.intValue();
-    } while (y <= 2 || intGCD != 1);
+    } while (intGCD != 1);
     return e;
   }
   public static BigInteger encrypt(BigInteger m, BigInteger e, BigInteger n) {
-    BigInteger x;
-    x = m.modPow(e, n);
-    return x;
+    return m.modPow(e, n);
   }
   public static BigInteger decrypt(BigInteger c, BigInteger d, BigInteger n) {
-    BigInteger y;
-    y = c.modPow(d, n);
-    return y;
+    return c.modPow(d, n);
   }
 }
