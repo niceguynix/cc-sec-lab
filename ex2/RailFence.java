080605e4cd5280a0da532ebcb2c039cb40c68ac0
niceguynix <its200manoj@gmail.com>
2023-11-01 20:51:47 +0530

    railfence

--- ex2/RailFence.java
+++ ex2/RailFence.java
@@ -4,40 +4,30 @@ class RailFence {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String msg = new String();
-    System.out.println("Enter the message:");
-    msg = sc.next();
-    int i, ptr;
-    char a[] = new char[20];
-    char b[] = new char[20];
     String enmsg = "";
     String pltxt = "";
-    i = 0;
-    for (ptr = 0; ptr < msg.length(); ptr = ptr + 2) {
-      a[i] = msg.charAt(ptr);
-      i++;
+
+    System.out.println("Enter the message:");
+    msg = sc.next();
+    sc.close();
+
+    int depth=2;
+
+    for(int i=0;i<depth;i++){
+      for(int j=i;j<msg.length();j+=depth){
+        enmsg+=msg.charAt(j);
+      }
     }
-    i = 0;
-    for (ptr = 1; ptr < msg.length(); ptr = ptr + 2) {
-      b[i] = msg.charAt(ptr);
-      i++;
+
+    double s=enmsg.length()*1.0/depth;
+    int x=(int) java.lang.Math.ceil(s);
+    for(int i=0;i<x;i++){
+      for(int j=i;j<enmsg.length();j+=x){
+        pltxt+=enmsg.charAt(j);
+      }
     }
-    i = 0;
-    for (ptr = 0; ptr < msg.length() / 2; ptr++) {
-      enmsg += a[i];
-      i++;
-    }
-    i = 0;
-    for (ptr = 0; ptr < msg.length() / 2; ptr++) {
-      enmsg += b[i];
-      i++;
-    }
-    System.out.println("Cipher Text:" + enmsg);
-    i = 0;
-    for (ptr = 0; ptr < msg.length() / 2; ptr++) {
-      pltxt += a[i];
-      pltxt += b[i];
-      i++;
-    }
-    System.out.println("Plain Text:" + pltxt);
+    System.out.println(enmsg);
+    System.out.println(pltxt);
+
   }
 }
