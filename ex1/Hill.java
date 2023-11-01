63be72f956d2289daefbd05aad23eca2b3d246a6
niceguynix <its200manoj@gmail.com>
2023-11-01 19:45:00 +0530

    hill syntax

--- ex1/Hill.java
+++ ex1/Hill.java
@@ -1,5 +1,4 @@
-import java.io.*;
-import java.lang.*;
+import java.util.Scanner;
 
 public class Hill {
   public static void main(String[] arg) throws Exception {
@@ -7,13 +6,17 @@ public class Hill {
     int p[] = new int[300];
     int c[] = new int[300];
     int i = 0;
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    Scanner sc = new Scanner(System.in);
     System.out.println("enter plain text");
-    String str = br.readLine();
+    String str = sc.nextLine();
+    sc.close();
+
     for (i = 0; i < str.length(); i++) {
       int c1 = str.charAt(i);
       p[i] = (c1) -97;
     }
+
     i = 0;
     int zz = 0;
     for (int b = 0; b < str.length() / 3; b++) {
@@ -25,6 +28,7 @@ public class Hill {
       }
       zz += 3;
     }
+
     System.out.println("Encrypted Text : ");
     for (int z = 0; z < str.length(); z++)
       System.out.print((char) ((c[z] % 26) + 97));
